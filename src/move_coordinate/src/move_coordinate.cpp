6649806db3161ec3c63a3e44d357c778035b0470
JeonB <newjeonb@gmail.com>
2022-11-22 14:58:58 +0900

    위치 초기화 추가

--- src/move_coordinate/src/move_coordinate.cpp
+++ src/move_coordinate/src/move_coordinate.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/move_group_interface/move_group_interface.h>
+#include <vector>
+
 int main(int argc, char *argv[])
 {
   // Initialize ROS and create the Node
@@ -35,18 +37,21 @@ int main(int argc, char *argv[])
   while (true)
   {
     char c;
-    std::cout << "\n좌표 입력: R / 관절각도 입력: T / 프로그램 종료: Q" << std::endl;
+    std::cout << "\n위치 초기화: R / 좌표 입력: I / 관절각도 입력: O / 프로그램 종료: Q" << std::endl;
 
     std::cin.clear();
     std::cin >> c;
-    if (c == 'q' || c == 'Q')
-    {
-      break; // q
-    }
-    else if (c == 't' || c == 'T')
+
+    if (c == 'r' || c == 'R')
     {
 
-      joint_group_positions[0] = -1.0; // radians
+      joint_group_positions[0] = 0.0;
+      joint_group_positions[1] = -0.8;
+      joint_group_positions[2] = 0.0;
+      joint_group_positions[3] = -2.4;
+      joint_group_positions[4] = 0.0;
+      joint_group_positions[5] = 1.6;
+      joint_group_positions[6] = 0.8;
       move_group_interface.setJointValueTarget(joint_group_positions);
 
       auto const [success, plan] = [&move_group_interface]
@@ -65,7 +70,7 @@ int main(int argc, char *argv[])
         RCLCPP_ERROR(logger, "Planning failed!");
       }
     }
-    else if (c == 'r' || c == 'R')
+    else if (c == 'i' || c == 'I')
     {
 
       // 목표 좌표 세팅
@@ -78,6 +83,11 @@ int main(int argc, char *argv[])
         std::cin >> msg.position.x >> msg.position.y >> msg.position.z;
         return msg;
       }();
+      //해당 좌표에 대한 관절 각도 출력
+      for (int i = 0; i < joint_group_positions.size(); i++)
+      {
+        printf("joint[%d] : %0.1lf\n", i, joint_group_positions[i]);
+      }
       move_group_interface.setPoseTarget(target_pose);
 
       // 목표 좌표에 대한 경로 생성
@@ -96,6 +106,38 @@ int main(int argc, char *argv[])
       {
         RCLCPP_ERROR(logger, "Planning failed!");
       }
+    }
+    else if (c == 'o' || c == 'O')
+    {
+      geometry_msgs::msg::Pose msg;
+      msg.orientation.w = 0.1;
+      // joint_group_positions[0] = -1.0; // radians
+      move_group_interface.setJointValueTarget(joint_group_positions);
+
+      for (int i = 0; i < joint_group_positions.size(); i++)
+      {
+        printf("joint[%d] : %0.1lf\n", i, joint_group_positions[i]);
+      }
+
+      auto const [success, plan] = [&move_group_interface]
+      {
+        moveit::planning_interface::MoveGroupInterface::Plan msg;
+        auto const ok = static_cast<bool>(move_group_interface.plan(msg));
+        return std::make_pair(ok, msg);
+      }();
+      // 경로 실행
+      if (success)
+      {
+        move_group_interface.execute(plan);
+      }
+      else
+      {
+        RCLCPP_ERROR(logger, "Planning failed!");
+      }
+    }
+    else if (c == 'q' || c == 'Q')
+    {
+      break; // q
     }
   }
   // Shutdown ROS
